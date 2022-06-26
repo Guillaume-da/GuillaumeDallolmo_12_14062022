2f69d0fb65b651c1bc492153d3ff4b7638ce7ba1
Guillaume-da <singy@hotmail.fr>
2022-06-26 17:45:19 +0200

    styled-components added to Message component

--- sportsee-react/src/components/Message/index.jsx
+++ sportsee-react/src/components/Message/index.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import './message.scss'
+import styled from 'styled-components'
+
+const SpanLabel = styled.span`
+    font-size: 48px;
+    line-height: 24px;
+`
+
+const RedSpanLabel = styled.span`
+    font-size: 48px;
+    color: #FF0101;
+    line-height: 24px;
+`
+
+const MessageLabel = styled.p`
+    margin-top: 48px;
+`
 
 const Message = (userName) => {
     
     return (
         <div>
-            <span className="main__title">Bonjour</span> <span className="main__title--red">{userName.userName}</span>
-            <p className="main__subtitle">Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+            <SpanLabel>Bonjour</SpanLabel> <RedSpanLabel>{userName.userName}</RedSpanLabel>
+            <MessageLabel>Félicitation ! Vous avez explosé vos objectifs hier 👏</MessageLabel>
         </div>
     );
 };
