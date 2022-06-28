71e40662f8f71d9cf7f09839da885add747e2c61
Guillaume-da <singy@hotmail.fr>
2022-06-28 17:58:08 +0200

    build done

--- sportsee-react/src/pages/Homepage.js
+++ sportsee-react/src/pages/Homepage.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import styled from 'styled-components'
 
-// import './header.scss'
-// import PropTypes from 'prop-types';
+const MainDivLabel = styled.div`
+    padding: 145px 70px 72px 210px;
+    height: 100%;
+    width: auto;
+    @media (min-width: 1024px) {
+      padding: 159px 34px 88px 160px;
+    }
+    @media (min-width: 1380px) {
+      padding: 159px 90px 88px 224px;
+    }
+`
 
 const Dashboard = () => {
     return (
-        <main className="main">
+        <MainDivLabel>
             <p>Homepage</p>
-        </main>
+        </MainDivLabel>
     );
 };
 
