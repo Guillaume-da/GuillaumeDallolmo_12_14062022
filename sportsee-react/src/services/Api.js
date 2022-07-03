5356d06fc2e5f094b5cf8749e45183e4c550683e
Guillaume-da <singy@hotmail.fr>
2022-07-03 12:10:57 +0200

    css corrections

--- sportsee-react/src/services/Api.js
+++ sportsee-react/src/services/Api.js
@@ -12,12 +12,12 @@ let mockedDatas = null
 apiBaseUrl.interceptors.response.use(function (response) {
   // Any status code that lie within the range of 2xx cause this function to trigger
   // Do something with response data
-  console.log(response.status)
+  // console.log(response.status)
   return response;
 }, function (error) {
   // Any status codes that falls outside the range of 2xx cause this function to trigger
   // Do something with response error
-  console.log(error)
+  // console.log(error)
   
   return Promise.reject(error);
 });
